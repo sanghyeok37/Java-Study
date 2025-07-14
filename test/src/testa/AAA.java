44a0e79a04bd683fe6de6f336bee19c30ae0175f
sanghyeok37 <hansh7hsh@naver.com>
2025-07-14 19:51:02 +0900

    test2

--- test/src/testa/AAA.java
+++ test/src/testa/AAA.java
@@ -5,7 +5,12 @@ public class AAA {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a= 1;
-		System.out.println(a);
+		System.out.println(a + 3f);   
+		
+		
+		
+		
+		
 	}
 
 }
